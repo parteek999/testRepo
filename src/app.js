2b9334905d3d4b5ce22e45be3256b0e539b44de2
parteek <web@aativa.com>
2022-06-29 23:41:41 +0530

    new code

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.set("view engine", "hbs");
 app.set('views', path.join(__dirname, '/../views'));
-// console.log(path.join(__dirname, '/../views'))
+console.log("hello neha")
 
 app.set("socket",socket)
 
